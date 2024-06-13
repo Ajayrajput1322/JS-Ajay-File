f0cdbe5ca85cc38e982ad75f24c0dea68467ad85
Ajay Singh <150016571+Ajayrajput1322@users.noreply.github.com>
2024-06-13 06:19:01 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+"use strict"; // treat all js code as new version
+
+ // alert ( 3 + 3) // we are using node js, not browser
+
+ console.log(3  
+    + 
+    3) // code readiabilty should be high //
+
+    console.log(Ajay);
+
+    let name = "Ajay"
+    let age = "19"
+    let
