fd37ef3cc9e9aaba29ad1aa41bce33d70c163815
Ajay Singh <150016571+Ajayrajput1322@users.noreply.github.com>
2024-06-14 12:52:51 +0000

    Stack and heap concept

--- .devcontainer/01_basics/.Datatypes-summary.js
+++ .devcontainer/01_basics/.Datatypes-summary.js
@@ -33,3 +33,26 @@ const myfunction = function(){
 
 }
 console.log(typeof outsidetemp);
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// stack (primitive), Heap (non-primitive)
+
+let Myyoutubename = "Ajayrajputdotcom"
+let anothername = Myyoutubename
+anothername = "codewithAjay"
+
+console.log(Myyoutubename);
+onsole.log(anothername);
+
+let username = {
+    email : "Ajay@gmail.com",
+    upi: "Ajay@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "Ajay123@gmail.com"
+console.log(userOne.email);
+console.log(userTwo.email);
