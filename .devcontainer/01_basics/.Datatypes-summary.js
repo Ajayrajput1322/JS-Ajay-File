3cc32442d0c5995d42b0d32814f27cef65c2fffc
Ajay Singh <150016571+Ajayrajput1322@users.noreply.github.com>
2024-06-14 06:20:38 +0000

    datatypes_summary

---
+++ .devcontainer/01_basics/.Datatypes-summary.js
@@ -0,0 +1,35 @@
+// primitive
+
+// 7 types : string, Number, Boolearn, null, undefined, symbol, Bigint
+
+const score = 100
+const scorevalue = 100.3
+
+const isLoggedIn = False
+const outsidetemp = null
+let userEmail;
+
+const id = symbol('123')
+const anotherId = symbol('123')
+
+console.log(id === anotherId);
+
+//const bigNumber = 3456785327678965n
+
+
+
+// Reference (Non primitive)
+
+// Array, objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name:"Ajay Rajput",
+    age: 20,
+}
+
+const myfunction = function(){
+    console.log("Hellow world");
+
+}
+console.log(typeof outsidetemp);
