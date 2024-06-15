18fb1c332aacc6a16c0ab495fb625a474cf2a0c7
Ajay Singh <150016571+Ajayrajput1322@users.noreply.github.com>
2024-06-15 08:08:12 +0000

    number and maths rules

---
+++ .devcontainer/01_basics/06_nums_and_maths.js
@@ -0,0 +1,34 @@
+const score = 400
+
+const balance = new Number(100)
+console.log(balance);
+
+
+// console.log(balance.tostring().length);
+// console.log(balance.tofixed(2));
+
+const otherNumber = 23.8966
+
+console.log(otherNumber.toprocesion(3));
+
+const hundreds = 1000000
+// console.log(hundreds.tolocalstrings('en-IN'));
+
+// ***************************maths****************8
+
+// console.log(Math);
+// console.log(math.abs(-4));
+// console.log(math.round(4.3));
+// console.log(math.ceil(4.5));
+// console.log(math.floor(4.9));
+// console.log(maths.Min(4, 3, 6, 8));
+// console.log(maths.max(4, 3, 6, 8));
+
+console.log(Math.random());
+console.log(Math.random()*10 + 1);
+console.log(Math.random()*10 + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
