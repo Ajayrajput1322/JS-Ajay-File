8d907fe042edbce56858c66252b9fc9d84039e52
Ajay Singh <150016571+Ajayrajput1322@users.noreply.github.com>
2024-06-16 17:51:33 +0000

    object 4 concept

---
+++ .github/02_basics/04_objects.js
@@ -0,0 +1,51 @@
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Rahul"
+tinderUser.isLoggedIn = False
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    Fullname: {
+        UserFullname: {
+            Firstname: "Ajay",
+            lastname: "Rajput"
+        }
+    }
+}
+
+console.log(regualrUser.Fullname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = { obj1,obj2 }
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+const obj3 = {...obj1, obj2}
+console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    }
+    {
+        id: 1,
+        email: "h@gmail.com"
+    }
+]
+
+users[1].email
+console.log(tinderUser);
+
+console.log(object.keys(tinderuser));
+console.log(object.values(tinderuser));
+console.log(object.entries(tinderuser));
+
+console.log(tinderUser.hasownproperty('isLoggedin'));
