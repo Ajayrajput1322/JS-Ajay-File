e2e525f3ef1a4e886203875711bb9b18f80a1b41
Ajay Singh <150016571+Ajayrajput1322@users.noreply.github.com>
2024-06-22 06:00:36 +0000

    loops iteration

---
+++ 05_iterations/one.js
@@ -0,0 +1,50 @@
+// for
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element == 5) {
+        console.log("5 is best number");
+    }
+    console.log(element);
+    
+}
+
+// console.log(element);
+
+for (let i = 0; i <= 10; i++) {
+    console.log('outer loop value: ${i}');
+    for (let j = 0; j <= 10; j++) {
+        // console.log('unner loop value ${j} and Inner loop ${j}');
+       // console.log(i + '*' j + ' = ' + i*j );
+    }
+
+}
+let myArray = ["flash", "batman", "superman"]
+console.log(myArray.length);
+for (let index = 0; index < myarray.length; index++) {
+    const element = array[index++];
+    console.log(element);
+    
+}
+
+
+//  Break and continue
+
+// for (let index = 1; index <= 20; index++) {
+// if (index == 5){
+ //       console.log('detected 5');
+  //      break
+   // }
+   // console.log('value pf 1 is ${index}');
+
+// }
+for (let index = 1; index <= 20; index++) {
+    if (index == 5){
+        console.log('detected 5');
+        break
+        continue
+    }
+    console.log('value pf 1 is ${index}');
+
+}
+
