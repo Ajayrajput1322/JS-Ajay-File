2ef06520e7e41e2e9fc16f23acc5ac8c8dc87554
Ajay Singh <150016571+Ajayrajput1322@users.noreply.github.com>
2024-06-23 05:07:14 +0000

    foreach

---
+++ 05_iterations/five.js
@@ -0,0 +1,39 @@
+const coding = ["js", "ruby", "java", "python", 'cpp']
+
+// coding.forEach( function (item) {
+ //   console.log(val);
+// } )
+
+// coding.forEach( (item) => {
+   // console.log(item);
+// } )
+
+// function printMe(item){
+ //    console.log(item);
+// }
+
+// coding.forEach(printMe())
+
+// coding.forEach( (item)=> {
+ //    console.log(item, index, arr);
+// })
+
+const mycoding = [
+    {
+        languagename: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languagename: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languagename: "javascript",
+        languageFileName: "js"
+    },
+]
+
+mycoding.forEach( (item) => {
+
+    console.log(item.languageName);
+})
---
+++ 05_iterations/four.js
@@ -0,0 +1,26 @@
+const myObject = { 
+    js: 'javacript'
+    cpp: 'c++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in object) {
+    console.log('${key} for shortcut is for ${myobject[key]}');
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    console.log(programming[key]);
+}
+
+// const map = new map()
+// map.set('IN', "India")
+// map.set('USA', "united states of america")
+// map.Set('fr', "france")
+//map.set('IN', "India")
+
+// for (const key in map) {
+  //  console.log(key);
+// }
